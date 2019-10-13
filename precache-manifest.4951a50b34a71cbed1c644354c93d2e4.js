658d4dda473b132397f3b74c2faa22c32f0adf81
PS-Ddevil <sinhapurushottam911@gmail.com>
2019-10-13 13:33:41 +0530

    Initialise Webpage

---
+++ precache-manifest.4951a50b34a71cbed1c644354c93d2e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33422aa623de3320bcce401c85469511",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c511a1d639f9b6a5e661",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "40cf4b30fd3c6ce03391",
+    "url": "/static/js/2.466cdbfa.chunk.js"
+  },
+  {
+    "revision": "c511a1d639f9b6a5e661",
+    "url": "/static/js/main.b33dd51c.chunk.js"
+  },
+  {
+    "revision": "b976d07bd505286beea2",
+    "url": "/static/js/runtime-main.393abd0a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
